5975ea54cf18bb2729ff0d5145905a7eccb560f9
Sheena Mae Pader <111573118+shynamay@users.noreply.github.com>
2022-08-19 21:46:18 +0800

    Add files via upload
    I submitted my Data types.

---
+++ MyDataTypes.java
@@ -0,0 +1,61 @@
+public class App {
+    public static void main(String[] args) {
+
+        // VARIABLES
+        String alaga_ko = "Mingming";
+        String codeusername = "Shyna";
+        String myFavoriteFictionalCharacter = "ShoutaKazehaya";
+        String myFavoriteMusic = "The Most BeautifulThing";
+        String myFavoriteShounenAnime = "One Piece";
+        String myFavoriteBL_Kdrama = "Semantic Error";
+
+        // int - stores integers (whole numbers), without decimals, such as 123 or -123
+        int myAge = 19;
+        int temp = 5;
+
+        // float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        float myGradeInClass = 4.00f;
+
+        // char - stores single characters, such as 'a' or 'B'. Char values are
+        // surrounded by single quotes
+        char myMiddleInitial = 'O';
+        char myFavoriteNumber = 13;
+
+        // boolean - stores values with two states: true or false
+        boolean iLoveMangoes = true;
+        boolean iAm7FeetTall = true;
+        
+        boolean Freshmankaba = true;
+        boolean Highschoolpabaako = false;
+        boolean Babypabaako = false;
+
+        // String - stores text, such as "Hello". String values are surrounded by double
+        // quotes
+        String myFirstName = "Sheena Mae";
+
+        // If you don't want others (or yourself) to overwrite existing values, use the
+        // final keyword (this will declare the variable as "final" or "constant", which
+        // means unchangeable and read-only)
+        final String myLastName = "Pader";
+
+        System.out.println(myAge);
+        System.out.println(myGradeInClass);
+        System.out.println(myMiddleInitial);
+        System.out.println(iLoveMangoes);
+        System.out.println(iAm7FeetTall);
+        System.out.println(myFirstName);
+        System.out.println(myLastName);
+        System.out.println(alaga_ko);
+        System.out.println(codeusername);
+        System.out.println(temp);
+        System.out.println(myFavoriteNumber);
+        System.out.println(myFavoriteFictionalCharacter);
+        System.out.println(myFavoriteMusic);
+        System.out.println(myFavoriteShounenAnime);
+        System.out.println(myFavoriteBL_Kdrama);
+        System.out.println(Freshmankaba);
+        System.out.println(Highschoolpabaako);
+        System.out.println(Babypabaako);
+
+    }
+}
